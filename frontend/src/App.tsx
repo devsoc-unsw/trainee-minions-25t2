19494eba03ea403821587ef88743848990136271
Andrew Chan <z5589701@ad.unsw.edu.au>
2025-09-13 20:18:28 +1000

    Initial work on matching algorithm

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,6 +8,7 @@ import HeroComponent from "./components/HeroComponent";
 import UserDashboard from "./Pages/user-dashboard";
 import EventDetails from "./components/EventDetails";
 import Quiz from "./Pages/quiz/quiz-page";
+import CompatibilityResults from "./Pages/compatibility-results";
 
 interface SearchFilters {
   events: string;
@@ -47,6 +48,7 @@ function App() {
         <Route path="/user-dashboard" element={<UserDashboard />} />
         <Route path="/event/:id" element={<EventDetails />} />
         <Route path="/quiz" element={<Quiz />} />
+        <Route path="/results" element={<CompatibilityResults />} />
       </Routes>
     </BrowserRouter>
   );
